f01eab6f8226208acb34990f20c046d3b13ef5ca
Remsey Mailjard <remsey@skills4-it.nl>
2026-05-11 23:25:42 +0200

    Beter examples

--- src/main/java/com/examples/models/Animal.java
+++ src/main/java/com/examples/models/Animal.java
@@ -1,47 +1,17 @@
 package com.examples.models;
 
-
-
-// This is the superclass (Parent class)
-
 public class Animal {
-	 String name;
-	private int age;
-
-
-	public Animal() {
-
-		System.out.println("Every animal will be constructed from the animal constructor class. This is the animal calling it");
-	}
+	private String name;
 
 	public Animal(String name) {
-		System.out.println("When you use the keyword super... it calls  the super constructor");
-		System.out.println("You know that " + name + " is a type of animal");
 		this.name = name;
+	}
 
+	public void makeSound() {
+		System.out.println(name + " makes a sound.");
 	}
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public void eat() {
-		System.out.println("This animal is eating.");
-	}
-
-	public void makeSound() {
-		System.out.println("This animal makes a sound.");
 	}
 }
